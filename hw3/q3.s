390a0557af771457e3381b6a1ade30474ed3e3ea
hoseony <hoseon.alex.yu@gmail.com>
2026-02-07 16:11:47 -0500

    updated to use v0

--- hw3/q3.s
+++ hw3/q3.s
@@ -1,40 +1,38 @@
-.data
-    result: .word 0
-
 .text
 .globl main
 
 calc_perimeter:
-    #t0 = a0 + a1
+    # t0 = a0 + a1
     add $t0, $a0, $a1
 
-    #t0 = t0 * 2
+    # t0 = t0 * 2
     sll $t0, $t0, 1
 
-    #result = t0
-    sw $t0, result
+    # $v0 = t0
+    move $v0, $t0
     
-    #return to main
-    jr $ra              #new discovery, from the textbook
+    # return to main
+    jr $ra              # new discovery, from the textbook
 
 main:
-    #int a0 = getchar()
+    # int a0 = getchar()
     li $v0, 5
     syscall
     move $a0, $v0
 
-    #int a1 = getchar()
+    # int a1 = getchar()
     li $v0, 5
     syscall
     move $a1, $v0
 
-    #calc_perimeter()
+    # calc_perimeter()
     jal calc_perimeter
-
+    
     #------------
-    #returns here
+    # returns here
     #------------ 
+
+    move $t2, $v0       # made to make sure the function is usable
 
     li $v0, 10
     syscall
-
